e1d9e1beeabe01c1cb20281ee4f15b68027a1a4e
Antonio Carpentieri <a.carpe@gmail.com>
2011-09-28 15:40:21 +0200

    completed walkthrough 6-2

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,2 +1,15 @@
 class HotelsController < ApplicationController
+  before_filter :load_hotel, :except => [:new, :index, :create]
+
+  def index
+    @hotels = Hotel.order(:name)
+  end
+
+  def show
+  end
+
+  protected
+  def load_hotel
+    @hotel =  Hotel.find(params[:id])
+  end
 end
