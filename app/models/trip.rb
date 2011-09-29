d1cf15f5c3081d0cadd06a7ad4e7f2f921c35666
Antonio Carpentieri <a.carpe@gmail.com>
2011-09-29 15:22:50 +0200

    added specs for trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,6 @@ class Trip < ActiveRecord::Base
   belongs_to :hotel
 
   validates :name, :uniqueness => true, :presence => true
+  validates :hotel, :presence => true
+  validates_length_of :name, :minimum => 2
 end
