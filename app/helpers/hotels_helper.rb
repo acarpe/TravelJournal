7e62d46b5d33d3ebf252ba90798af46d7be10da9
Antonio Carpentieri <a.carpe@gmail.com>
2011-09-29 10:50:10 +0200

    half lab 8

--- app/helpers/hotels_helper.rb
+++ app/helpers/hotels_helper.rb
@@ -2,4 +2,14 @@ module HotelsHelper
   def description_column description
     content_tag :td, truncate(description,lenght: 25), title: description
   end
+
+  def star_ratings
+    [
+      {description: "Perfect", value: 5},
+      {description: "Great", value: 4},
+      {description: "Good", value: 3},
+      {description: "Fair", value: 2},
+      {description: "Poor", value: 1}
+    ]
+  end
 end
